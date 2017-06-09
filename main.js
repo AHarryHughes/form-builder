e9d65a4ece6c47eec7d70b1474f98fe91c65a503
AHarryHughes <Ahhughes8@gmail.com>
2017-06-09 10:58:33 -0400

    closer but not perfect

--- main.js
+++ main.js
@@ -1,167 +1,127 @@
-var formData = [
-  {
-    "type": "text",
-    "label": "First Name",
-    "id": "user-first-name",
-    "icon": "fa-user",
-    "options": []
-  },
-  {
-    "type": "text",
-    "label": "Last Name",
-    "id": "user-last-name",
-    "icon": "fa-user",
-    "options": []
-  },
-  {
-    "type": "email",
-    "label": "Email Address",
-    "id": "user-email",
-    "icon": "fa-envelope",
-    "options": []
-  },
-  {
-    "type": "text",
-    "label": "Current website url",
-    "id": "user-website",
-    "icon": "fa-globe",
-    "options": []
-  },
-  {
-    "type": "select",
-    "label": "Select Language",
-    "id": "user-language",
-    "icon": "",
-    "options": [
-      {
-        "label": "English",
-        "value": "EN"
-      },
-      {
-        "label": "French",
-        "value": "FR"
-      },
-      {
-        "label": "Spanish",
-        "value": "SP"
-      },
-      {
-        "label": "Chinese",
-        "value": "CH"
-      },
-      {
-        "label": "Japanese",
-        "value": "JP"
-      }
-    ]
-  },
-  {
-    "type": "textarea",
-    "label": "Your Comment",
-    "id": "user-comment",
-    "icon": "fa-comments",
-    "options": []
-  },
-  {
-    "type": "tel",
-    "label": "Mobil Number",
-    "id": "user-mobile",
-    "icon": "fa-mobile-phone",
-    "options": []
-  },
-  {
-    "type": "tel",
-    "label": "Home Number",
-    "id": "user-phone",
-    "icon": "fa-phone",
-    "options": []
-  }
-];
 
 
-/*
 
-var todoInput = document.querySelector("#todoInput");
-
-var todoList = document.querySelector("#todoList");
-
-todoInput.addEventListener('keypress', function(event){
-
-  if (event.keyCode == 13){
-    console.log(event.target.value);
-    todoContent = event.target.value;
-    console.log(todoContent);
-    event.target.value = "";
-    console.log(event.target.value);
-    var newTodoLi = document.createElement('li');
-    console.log(newTodoLi);
-    newTodoLi.textContent = todoContent;
-    console.log(newTodoLi);
-    todoList.appendChild(newTodoLi);
-    console.log(todoList);
-  }
-  return event;
-});
-
-*/
-
-//import {formData} from './form-data';
+//HTML--------------------------------------------------------------------------
 
 
+//grab body and css
 var bod = document.querySelector(".bod");
+bod.style.margin= "auto";
+bod.style.width= "400px";
+bod.style.boxShadow= "5px 5px grey";
+bod.style.borderRadius= "5px";
 
+//create and save to var: head, main, and footer and add css
 var daHead = document.createElement('header');
+daHead.style.backgroundColor= "rgb(56,139,199)";
+daHead.style.color= "white";
+daHead.style.borderRadius= "5px";
+daHead.style.padding= ".005% 4%";
 var daMain = document.createElement('main');
+daMain.style.padding= "0% 4% 5% 4%";
 var daFoot = document.createElement('footer');
+daFoot.style.padding= "6% 4%";
+daFoot.style.backgroundColor= "rgb(199,222,239)";
+daFoot.style.borderRadius= "5px";
 
+//attatch head, main, and footer to body
 bod.appendChild(daHead);
 bod.appendChild(daMain);
 bod.appendChild(daFoot);
 
-var daTitle = document.createElement('h1');
+//create h1, put text in it and attach to head
+var daTitle = document.createElement('h2');
 daTitle.textContent = "Sign Up For My Web App";
-
+daTitle.style.fontWeight= "lighter";
 daHead.appendChild(daTitle);
 
 //<label>Click me <input type="text"></label>
 
-
+//for loop to make the divs for the input feilds
 for (var i in formData){
+  //create div, add class named after the label in formdata and attach to main
   var daDiv = document.createElement('div');
   daDiv.classList = formData[i].label;
   daMain.appendChild(daDiv);
 
 
 
-
+  //conditional for select in formdata
   if(formData[i].type=="select"){
+    //create select element
     var daSelect = document.createElement('select');
+    daSelect.style.margin= "2% 0%";
+    //for loop for options in formdata
     for (var j in formData[i].options){
+      //create option element, add label from formdata to textContent attribute,
+      //add label from formdata to value attribute,
+      //and attatch option to select
       var daOption = document.createElement('option');
       daOption.textContent = formData[i].options[j].label;
-      daOption.setAttribute(value, formData[i].options[j].value);
+      daOption.setAttribute("value", formData[i].options[j].value);
       daSelect.appendChild(daOption);
     }
+    //attatch select to div
+    daDiv.appendChild(daSelect);
   }
 
-  else if(formData[i].type=="textarea"){
-    var daText = document.createElement('textarea');
-    daText.setAttribute(rows, 10);
-    daText.setAttribute(cols, 20);
 
+  //conditional for textarea in formdata
+  else if(formData[i].type=="textarea"){
+    //create textarea, add row and col attribute, and attatch textarea to div
+    var daText = document.createElement('textarea');
+    daText.setAttribute("value", formData[i].label);
+    daText.setAttribute("rows", 5);
+    daText.setAttribute("cols", 55);
+    daText.style.margin= "2% 0%";
     daDiv.appendChild(daText);
   }
 
+  //conditional for the rest of the inputs and add css
   else{
+    //Create span element for icon in formdata, add class Icon to div,
+    //and attatch Icon Span to parent div
+    var daSpan = document.createElement('span');
+    daSpan.classList = "Icon";
+    daDiv.appendChild(daSpan);
+
+    //Create icon element for icon in formdata, add class Icon to icon from formdata,
+    //and attatch Icon to span
+    var daIcon = document.createElement('i');
+    daIcon.classList = "fa " + formData[i].icon;
+    daSpan.appendChild(daIcon);
+    daIcon.style.color= "rgb(160,165,167)";
+    daIcon.style.position= "absolute";
+    daIcon.style.margin= "1.5% 0% 2.5% 0%";
+    daIcon.style.left= "22%";
+
+    //Create input element, set type attribute from type in formdata to input,
+    //and set value attribute from value in formdata to input,
     var daInput = document.createElement('input');
-    daInput.setAttribute(type, formData[i].type);
-    daInput.setAttribute(value, formData[i].icon+formData[i].label);
+    daInput.classList = formData[i].type;
+    daInput.setAttribute("type", formData[i].type);
+    daInput.setAttribute("value", formData[i].label);
+    daInput.style.width= "80%";
+    daInput.style.margin= "2% 0%";
+    daInput.style.padding= "1% 7%";
 
+    //Create label element and set for attribute from for in formdata to label
     var daLabel = document.createElement('label');
-    daLabel.setAttribute(for, formData[i].label);
+    daLabel.setAttribute("for", formData[i].id);
 
+    //Attatch label to div and attatch input to label
     daDiv.appendChild(daLabel);
     daLabel.appendChild(daInput);
   }
 
 }
+
+//create button, put text in it and attach to foot
+var daButton = document.createElement('button');
+daButton.textContent = "Submit Form";
+daFoot.appendChild(daButton);
+
+//CSS---------------------------------------------------------------------------
+
+
+//newTodoLi.style.padding='5%';
